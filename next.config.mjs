13533c2e47390a6bc9848a4cb9fb96b5ef5ab697
paul.pham <paul.pham@code88.asia>
2024-02-22 11:14:13 +0700

    additional commmit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   optimizeFonts: false,
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  }
 };
 
 export default nextConfig;
