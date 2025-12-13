021bd7273ecacb30f7244a38e24c9df02dfcb342
tuanpc1902 <tuanpc.1902@gmail.com>
2025-12-13 22:44:28 +0700

    Add max-width screen sizes to Tailwind configuration for better responsiveness

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,13 +28,19 @@ const config: Config = {
       },
     },
     screens: {
-      'max-xs': '400px',
       'xs': '480px',
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
+      // max width
+      'max-xs': {max: '390px'},
+      'max-sm': {max: '639px'},
+      'max-md': {max: '767px'},
+      'max-lg': {max: '1023px'},
+      'max-xl': {max: '1279px'},
+      'max-2xl': {max: '1535px'},
     },
       // => @media (min-width: 640px) { ... }
   },
